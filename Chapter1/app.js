79a18c832e4b3649fef46b3748cab029ad333f6a
taiga07 <taigatani@me.com>
2022-01-29 22:28:55 +0900

    [practice]JS

---
+++ Chapter1/app.js
@@ -0,0 +1,21 @@
+// alert('Hello World');
+
+// let hello = 'Hello World';
+// alert(hello);
+
+// alert('Hello'+'World');
+
+// let str1 = 'Hello';
+// let str2 = 'World!!';
+// alert(str1+str2);
+
+let orange = 110;
+let apple = 120;
+
+if(orange<apple){
+  alert('みかんの値段がリンゴより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がリンゴより高い');
+}
