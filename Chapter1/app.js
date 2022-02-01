eb4bc254dbe70a6771533e9c511f076fc40efb72
taiga07 <taigatani@me.com>
2022-02-01 21:47:56 +0900

    [practice]JS

--- Chapter1/app.js
+++ Chapter1/app.js
@@ -9,13 +9,25 @@
 // let str2 = 'World!!';
 // alert(str1+str2);
 
-let orange = 110;
-let apple = 120;
+// let orange = 110;
+// let apple = 120;
 
-if(orange<apple){
-  alert('みかんの値段がリンゴより安い');
-} else if(orange == apple){
-  alert('みかんとりんごが同じ値段');
-} else{
-  alert('みかんの値段がリンゴより高い');
+// if(orange<apple){
+//   alert('みかんの値段がリンゴより安い');
+// } else if(orange == apple){
+//   alert('みかんとりんごが同じ値段');
+// } else{
+//   alert('みかんの値段がリンゴより高い');
+// }
+
+
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num<max){
+  num = num*2;
+  count = count+1;
 }
+
+alert('2を掛け続けて'+max+'を越えるのに必要だった回数は'+count+'回です');
