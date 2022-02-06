686a7c7583f35777da1d17d2ba308a0699712002
taiga07 <taigatani@me.com>
2022-02-06 23:22:50 +0900

    [ptactis]JS

--- Chapter1/app.js
+++ Chapter1/app.js
@@ -21,13 +21,22 @@
 // }
 
 
-let max = 100;
-let num = 1;
-let count = 0;
+// let max = 100;
+// let num = 1;
+// let count = 0;
 
-while(num<max){
-  num = num*2;
-  count = count+1;
+// while(num<max){
+//   num = num*2;
+//   count = count+1;
+// }
+
+// alert('2を掛け続けて'+max+'を越えるのに必要だった回数は'+count+'回です');
+
+let i;
+let num = 0;
+
+for(i=1;i<11;i++){
+  num = num + i;
 }
 
-alert('2を掛け続けて'+max+'を越えるのに必要だった回数は'+count+'回です');
+alert('1から10まで足し算した結果は'+num+'です');
