9e0e0f6cbc0da6aa8d98993da902c6462af7f517
taiga07 <taigatani@me.com>
2022-02-09 23:35:15 +0900

    [ptactice]

---
+++ Chapter1/sample.js
@@ -0,0 +1,14 @@
+// var nickname = "taro"
+// console.log(nickname)
+// var nickname = "ichiro"
+// console.log(nickname)
+
+// let nickname = "taro"
+// console.log(nickname)
+// let nickname = "ichiro"
+// console.log(nickname)
+
+const nickname = "taro"
+console.log(nickname)
+const nickname = "ichiro"
+console.log(nickname)
