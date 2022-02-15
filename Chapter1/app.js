4d2b4f5f986695edb742b90660e16fb10a730263
taiga07 <taigatani@me.com>
2022-02-15 22:48:47 +0900

    [practice]

--- Chapter1/app.js
+++ Chapter1/app.js
@@ -51,6 +51,10 @@
 //   return addStr;
 // }
 
-let promptStr = prompt('何か好きな文字を入力してください。');
+// let promptStr = prompt('何か好きな文字を入力してください。');
 
-alert(promptStr);
+// alert(promptStr);
+
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+
+alert('あなたの選んだ手は'+user_hand+'です。');
